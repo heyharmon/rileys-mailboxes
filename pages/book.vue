cbcd8af02a0c3b34dd1427babf94d7f1fad613d9
Ryan Harmon <ryan@bloomcu.com>
2025-07-09 13:08:15 -0600

    feat: update consultation icon to calendar design in booking page

--- pages/book.vue
+++ pages/book.vue
@@ -183,7 +183,7 @@
           <div class="p-6">
             <div class="w-16 h-16 bg-[#e9ca5d] rounded-full flex items-center justify-center mx-auto mb-4">
               <svg class="w-8 h-8 text-gray-800" fill="currentColor" viewBox="0 0 20 20">
-                <path d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"/>
+                <path d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zM4 8h12v8H4V8zm2 2v2h2v-2H6zm4 0v2h2v-2h-2z"/>
               </svg>
             </div>
             <h3 class="text-xl font-bold text-gray-800 mb-3">Free Consultation</h3>
